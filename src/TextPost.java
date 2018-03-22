aae842131a4b1591d8e2897debfe3c2c31478179
Espen <espenk95@gmail.com>
2018-03-22 01:02:33 +0100

    Add text post, sub class of post.

---
+++ src/TextPost.java
@@ -0,0 +1,36 @@
+/**
+ * This class stores information about a text post.
+ * Sub class of post.
+ *
+ * @author Espen Kvernstad
+ * @version 2018-03-21
+ */
+public class TextPost extends Post {
+    private final String message;
+
+    /**
+     * Constructor for the text post class.
+     * @param author the username of the author of the post.
+     * @param message the message text.
+     */
+    public TextPost(String author, String message) {
+        super(author);
+        this.message = message;
+    }
+
+    /**
+     * Return the message text of the post.
+     * @return the message text of the post.
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Display information about this post.
+     */
+    public void display() {
+        System.out.println("\t" + getMessage());
+        super.display();
+    }
+}
