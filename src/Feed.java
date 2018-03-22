5648a0b0220e9b8e7ac0fcc2d7550e37b896cc22
Espen <espenk95@gmail.com>
2018-03-22 01:03:18 +0100

    Add feed class to manage the posts.

---
+++ src/Feed.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+        import java.util.Iterator;
+
+public class Feed {
+    private final ArrayList<Post> posts;
+
+    /**
+     * Constructor for the feed class.
+     */
+    public Feed() {
+        this.posts = new ArrayList<>();
+    }
+
+    /**
+     * Add a post to the feed.
+     * @param post the post to add to the feed.
+     */
+    public void addPost(Post post) {
+        posts.add(post);
+    }
+
+    /**
+     * Display all the posts in the feed.
+     */
+    public void display() {
+        for (Post post : posts) {
+            post.display();
+            System.out.println();  // empty line between posts
+        }
+    }
+
+    /**
+     * Return the iterator of the posts.
+     * @return the iterator of the posts.
+     */
+    public Iterator<Post> getIterator() {
+        return posts.iterator();
+    }
+}
